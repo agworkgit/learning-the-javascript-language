26e52a75982e9abd29a2ae52dc3e0689dc52f054
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-02-26 15:23:29 +0000

    finished comparisons lesson

---
+++ Exercises/13_comparisons.js
@@ -0,0 +1,13 @@
+if (10 % 2 === 0)
+{
+    console.log('10 modulo 2 is 0');
+} else {
+    console.log('number modulo 2 is 1');
+    
+}
+
+console.log(10 < 2); // false
+console.log(2 >= 1); // true
+console.log(55 % 2 === 1); // true
+
+
